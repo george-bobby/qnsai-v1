4bda745a419f7bee198e5f57391887a244bf483e
George Bobby <mailto.georgebobby@gmail.com>
2024-09-20 15:14:05 +0530

    venv

--- server/build_files.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo "BUILD START"
-
-# Upgrade pip and install requirements
-python3 -m pip install --upgrade pip
-python3 -m pip install --upgrade -r requirements.txt --disable-pip-version-check --no-cache-dir
-
-echo "BUILD END"
