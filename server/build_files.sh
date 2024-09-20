8c405d3e0728cf3b3adb4d19bc0f541bcd1c01a1
George Bobby <mailto.georgebobby@gmail.com>
2024-09-20 14:57:46 +0530

    vercel 3

--- server/build_files.sh
+++ server/build_files.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 echo "BUILD START"
-python3 -m pip install -r requirements.txt  # Using python3's pip explicitly
+
+# Upgrade pip and install requirements
+python3 -m pip install --upgrade pip
+python3 -m pip install --upgrade -r requirements.txt --disable-pip-version-check --no-cache-dir
+
 echo "BUILD END"
